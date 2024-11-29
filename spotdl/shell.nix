33a419290f1db41077cb3680e49cff01e95095fb
mimvoid <mimvoid@proton.me>
2024-11-28 20:37:25 -0500

    add shell scripts that work like aliases for any shell

--- spotdl/shell.nix
+++ spotdl/shell.nix
@@ -1,12 +1,26 @@
 { pkgs ? import <nixpkgs> {} }:
-
+let
+  aliases = {
+    # Download in OPUS format with lyrics
+    spotdl-opus = pkgs.writeShellScriptBin "spotdl-opus"
+      ''
+        spotdl --audio {youtube-music,youtube} \
+        --lyrics {musixmatch,synced,genius,azlyrics} --generate-lrc \
+        --bitrate disable \
+        --format opus \
+        "$@"
+      '';
+  };
+in
 pkgs.mkShell {
   buildInputs = with pkgs; [
     spotdl
+  ]
+  ++ [
+    aliases.spotdl-opus
   ];
 
   shellHook = ''
     echo -e "Basic usage: spotdl <song url>\nDocumentation: https://spotdl.readthedocs.io"
-    alias spotdl="spotdl --audio {youtube-music,youtube} --lyrics {musixmatch,synced,genius,azlyrics} --generate-lrc --bitrate disable --format opus --restrict ascii"
   '';
 }
--- yt-dlp/shell.nix
+++ yt-dlp/shell.nix
@@ -1,15 +1,34 @@
 { pkgs ? import <nixpkgs> {} }:
+let
+  # Remove unneeded dependencies
+  yt-dlp = pkgs.yt-dlp.override {
+    rtmpSupport = false;
+  };
 
+  aliases = {
+    # Download audio-only with aria2c
+    yt-audio = pkgs.writeShellScriptBin "yt-audio"
+      ''
+        yt-dlp \
+        --downloader aria2c \
+        --check-formats -x --audio-format opus \
+        --no-embed-chapters --no-embed-info-json \
+        --embed-metadata --parse-metadata ':(?P<meta_synopsis>)' \
+        --write-subs --sub-langs 'en.*,ja' --convert-subs lrc \
+        --embed-thumbnail --convert-thumbnails jpg \
+        "$@"
+      '';
+  };
+in
 pkgs.mkShell {
-  buildInputs = with pkgs; [
-    (yt-dlp.override {
-      atomicparsleySupport = false;
-      rtmpSupport = false;
-    })
+  name = "yt-dlp shell";
+
+  packages = [
+    yt-dlp
+    aliases.yt-audio
   ];
 
   shellHook = ''
     echo -e "Download OPUS audio with jpg thumbnail: yt-audio <url>"
-    alias yt-audio="yt-dlp --downloader aria2c --check-formats -x --audio-format opus --embed-thumbnail --embed-metadata --no-embed-chapters --no-embed-info-json --parse-metadata ':(?P<meta_synopsis>)' --write-subs --sub-langs 'en.*,ja' --convert-subs lrc --convert-thumbnails jpg"
   '';
 }
