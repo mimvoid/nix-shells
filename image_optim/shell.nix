1ee118cda7cb64a366b008dbb755c2eba988823c
mimvoid <mimvoid@proton.me>
2024-11-28 20:54:37 -0500

    move image_optim config to its own file

--- image_optim/shell.nix
+++ image_optim/shell.nix
@@ -6,19 +6,7 @@ pkgs.mkShell {
   packages = with pkgs; [ image_optim ];
 
   shellHook =
-    let
-      image_optim_config = builtins.toFile "image_optim.yaml" # yaml
-        ''
-          allow_lossy: false
-          verbose: true
-          timeout: 120
-          gifsicle:
-            careful: true
-          jpegoptim:
-            strip: ['com', 'iptc', 'icc', 'xmp']
-        '';
-    in
     ''
-      ln -s ${image_optim_config} ~/.config/image_optim.yaml
+      ln -s ${./image_optim.yaml} ~/.config/image_optim.yaml
     '';
 }
